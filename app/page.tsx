9aa5d5d3ef42660bf56604fef8c32629282766d6
Muhammad Ahsan <idahsanali@gmail.com>
2024-10-07 16:40:39 +0500

    small changes file

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
    <>
    <nav>
-    <ul style={{display:"flex",gap:"2rem",listStyle:"none",fontSize:"3rem",justifyContent:"center",fontWeight:"bolder",color:"red"}}>
+    <ul style={{display:"flex",gap:"2rem",listStyle:"none",fontSize:"3rem",justifyContent:"center",fontWeight:"bolder",color:"red",backgroundColor:"black"}}>
      <li><Link href={'/Home'}>Home</Link></li>
      <li><Link href={'/About'}>About</Link></li>
      <li><Link href={'/Service'}>Services</Link></li>
